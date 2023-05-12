7c2fe37bd79a839496fc3379c2861c554f65ab2b
Cozark <longho250702@gmail.com>
2023-05-13 01:39:00 +0700

    fix

--- video-editing-api/Program.cs
+++ video-editing-api/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace video_editing_api
@@ -18,6 +19,17 @@ namespace video_editing_api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                        .UseKestrel(options =>
+                        {
+                            // Heroku sets the PORT environment variable, and .NET uses the HTTP_PORT variable.
+                            string port = Environment.GetEnvironmentVariable("PORT") ?? "44394";
+                            Environment.SetEnvironmentVariable("ASPNETCORE_URLS", $"http://*:{port}");
+
+                            options.Listen(IPAddress.Any, int.Parse(port));
+                        });
+                });
     }
 }
